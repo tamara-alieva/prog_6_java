7430db4f2b3e97a6b6979f122a7776b716b8ac8f
Tamara <tamaraavgust14@gmail.com>
2024-01-04 18:09:01 +0700

    Тест вызова конструктора базового класса

--- Main.java
+++ Main.java
@@ -6,14 +6,18 @@ public class Main {
         System.out.println("ТЕСТЫ:");
         System.out.println("1) Перегруженный метод setInfo(...) в классе Passenger (с вызовом метода класса Person)");
         Passenger passenger1 = new Passenger();
-        passenger1.setInfo("Ivan", 5000, false);
+        passenger1.setInfo("Иван", 5000, false);
         passenger1.output();
 
         //System.out.println();
         System.out.println("2) Перегруженный метод setInfo(...) в классе Driver (без вызова метода класса Person)");
         Driver driver1 = new Driver();
-        driver1.setInfo("Leonid", 8700, 14, 390);
+        driver1.setInfo("Леонид", 8700, 14, 390);
         driver1.output();
 
+        System.out.println("3) Вызов конструктора базового класса из конструктора производного класса");
+        Passenger passenger2 = new Passenger("Георгий", 5000, true);
+        passenger2.output();
+        
     }
 }
